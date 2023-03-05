8817c09cbcf5957c71b42ae9dd6f08e6af045270
v836178 <Amit.bhati@united.com>
2023-03-05 17:22:14 +0530

    pallendirom program adding

---
+++ src/test/java/JavaPrograms/PallendiromProgram.java
@@ -0,0 +1,40 @@
+package JavaPrograms;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+public class PallendiromProgram {
+	
+	
+	@Test
+	
+	public void Pallendiromprogrm() {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int num=sc.nextInt();
+		
+		int rev=0;
+		int f=num;
+		
+		while(num>0) {
+			
+			
+			int remind=num%10;
+			
+			rev=rev*10+remind;
+			num=num/10;
+			
+		}if(f==rev) {
+			
+			System.out.println("number is pallendirom"+rev);
+		}else {
+			
+			System.out.println("number is not pallendirom"+rev);
+			
+		}
+		
+	}
+
+}
